5e57da83d14ebb25c84db8ca6a3dca978ff0492d
Demosfenys <Demosfenys@yandex.ru>
2023-03-05 22:06:25 +0300

    test1

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"GopherMart2/internal/router"
+)
+
+func main() {
+	rout := router.InitServer()
+	err := rout.Router()
+
+	if err != nil {
+		log.Fatal("Router:", err)
+	}
+}
